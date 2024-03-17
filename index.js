97018e2d363ddf809ada9bd54e342848f541590f
Timothy Chico Supan <timsupann@gmail.com>
2024-03-17 11:37:16 -0700

    Add showSlides function to move back and forth between images

--- index.js
+++ index.js
@@ -15,5 +15,18 @@ function showSlides(n){
     let i;
     let slides = document.querySelector(".mySlides");
     let dots = document.querySelector(".dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < slides.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
 
